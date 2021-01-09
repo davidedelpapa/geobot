a655435c0c7f2968109ce672731a326e843a01e5
davidedelpapa <davidedelpapa@yahoo.com>
2021-01-09 22:07:13 +0100

    fixed errors in app.py

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 from geobot.tileget import Tileget
 from geobot.tilerender import draw_image
 from geobot.geo import Bbox
 import os
+from shutil import rmtree
 app = Flask(__name__)
 
 @app.route('/')
 def root_response():
     return 'Hello from DO App Platform!'
 
-@app.route('/api/(<float:w>,<float:n>,<float:e>,<float:s>)')
+@app.route('/api/bbox/<float:w>/<float:n>/<float:e>/<float:s>/')
 def show_bbox(w, n, e, s):
     ''' Shows an Image contaning the selected bounding-box '''
     bbox = Bbox(w, n, e, s)
@@ -22,12 +23,12 @@ def show_bbox(w, n, e, s):
     out = draw_image(bbox, getter, watermark="© Mapbox", crop_bbox=cropped)
 
     path = out.path
-    def generate_response():
-        with open(path) as f:
-            yield from f
-
-        os.remove(path)
-
-    r = app.response_class(generate_response(), mimetype=f"image/{out.ext}")
-    r.headers.set('Content-Disposition', 'attachment', filename=f"{out.name}{out.ext}")
-    return r
+    out_dir = out.dir
+    
+    with open(path, 'rb') as f:
+        data = f.readlines()
+    out.remove()
+    rmtree(out_dir)
+    return Response(data, headers={
+        'Content-Type': f"image/{out.ext}"
+    })
